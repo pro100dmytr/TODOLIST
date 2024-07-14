bbf6872e96b49bbd18eb61c28531d8b432527fd4
Dmytro <dima25122018@gmail.com>
2024-07-14 14:24:54 +0300

    version after deletion

--- internal/http-server/handlers/get/get.go
+++ internal/http-server/handlers/get/get.go
@@ -12,7 +12,7 @@ import (
 func GetTodos(c echo.Context, store *postgresql.Storage) error {
 	rows, err := store.GetTodoItem()
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, create.ErrorResponse{Error: "Помилка при отриманні завдань"})
+		return c.JSON(http.StatusInternalServerError, create.ErrorResponse{Error: "Error when receiving tasks"})
 	}
 	defer rows.Close()
 
@@ -22,13 +22,13 @@ func GetTodos(c echo.Context, store *postgresql.Storage) error {
 		var task model.Todo
 		err := rows.Scan(&task.ID, &task.Title, &task.Completed)
 		if err != nil {
-			return c.JSON(http.StatusInternalServerError, create.ErrorResponse{Error: "Помилка при скануванні рядків"})
+			return c.JSON(http.StatusInternalServerError, create.ErrorResponse{Error: "Error when scanning strings"})
 		}
 		tasks = append(tasks, task)
 	}
 
 	if err := rows.Err(); err != nil {
-		return c.JSON(http.StatusInternalServerError, create.ErrorResponse{Error: "Помилка при ітерації по результатам запиту"})
+		return c.JSON(http.StatusInternalServerError, create.ErrorResponse{Error: "Error when iterating on query results"})
 	}
 
 	return c.JSON(http.StatusOK, tasks)
